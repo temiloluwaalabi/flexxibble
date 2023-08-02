3f2f2e8effcbe1677fc7a3bb3d0e9efbb9a8c89a
Alabi Temiloluwa Emmanuel <59064504+temiloluwaalabi@users.noreply.github.com>
2023-08-02 18:50:51 +0100

    Update session.ts

--- lib/session.ts
+++ lib/session.ts
@@ -1,6 +1,6 @@
 import {getServerSession} from 'next-auth/next';
 import { NextAuthOptions, User} from 'next-auth';
-import { AdapterUser } from 'next-auth/adapters';
+import { AdapterUser } from '../node_modules/next-auth/src/adapters';
 import GoogleProvider from 'next-auth/providers/google';
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT } from 'next-auth/jwt';
